72e16f74bcc2bb8886766a191da2f1760ca5e9d4
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-10-16 18:40:45 -0400

    Create Student model

---
+++ models/student.js
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+const mongoose = require("./connection");
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// Pull schema and model from mongoose
+const { Schema, model } = mongoose
+
+// Make summaries schema
+const studentsSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  dateOfBirth: Date,
+  homeAddress: String,
+  enrollmentStatus: String,
+  enrollmentDate: Date
+})
+
+
+// Make Student model
+const Student = model("Student", studentsSchema)
+
+////////////////////////////////////////////////
+// Export model
+////////////////////////////////////////////////
+module.exports = Student
